05585ad66a8e3a7955190a67efc86fcd4d1595ff
huangzhenran <1214084237@qq.com>
2019-05-10 10:33:06 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Goddess_s_Wardrode.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
