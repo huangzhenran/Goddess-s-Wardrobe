6f004440e471764ee0e4fd27cc9b441e2f4243d8
huangzhenran <1214084237@qq.com>
2019-05-24 11:21:26 +0800

    add

---
+++ Goddess_s_Wardrode1/Models/FashionWomen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Goddess_s_Wardrode1.Models
+{
+    public class FashionWomen
+    {
+        public string Picture { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
